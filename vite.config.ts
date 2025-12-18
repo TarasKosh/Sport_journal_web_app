f1e60a2206212ad9489ea6c9a31c8f5cc43436ae
Taras <taras.koshelev.public@gmail.com>
2025-12-18 23:17:30 +0100

    PWA added

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      injectRegister: null,
+      registerType: 'autoUpdate',
+      includeAssets: ['vite.svg'],
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'strength-journal',
+        short_name: 'Journal',
+        start_url: '.',
+        scope: '.',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'vite.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
+          },
+        ],
+      },
+    }),
+  ],
 })
